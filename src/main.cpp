4ce37b02c51041433899d12d89c00b1817d22d40
WizardMD <wizardiche@mail.ru>
2021-08-16 00:20:21 +0500

    An additional storage for results instead of sort action

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 #include <cassert>
 #include <string>
 #include <vector>
@@ -43,6 +42,7 @@ static string GetStringFromStream(const string &message) {
     return str;
 }
 #else
+
 static double GetDoubleFromStream(const string &message, double min, double max) {
     static int count = 0;
     double speeds[NUM_OF_SWIMMERS] = {
@@ -50,7 +50,7 @@ static double GetDoubleFromStream(const string &message, double min, double max)
     };
     cout << speeds[count] << endl;
     double speed = speeds[count];
-    if (++count > NUM_OF_SWIMMERS ) {
+    if (++count > NUM_OF_SWIMMERS) {
         count = 0;
     }
 
@@ -64,56 +64,73 @@ static string GetStringFromStream(const string &message) {
     };
     cout << names[count] << endl;
     string name = names[count];
-    if (++count > NUM_OF_SWIMMERS ) {
+    if (++count > NUM_OF_SWIMMERS) {
         count = 0;
     }
 
     return name;
 }
+
 #endif
+
+class ResultsStorage {
+    vector<double> items;
+    mutex dataAccess;
+public:
+    void PushItem(double item) {
+        dataAccess.lock();
+        items.push_back(item);
+        dataAccess.unlock();
+    }
+
+    const vector<double> &GetStorage() const {
+        return items;
+    }
+};
 
 class Swimmer {
     string name;
     double speed;
-    double duration = 0;
     double restOfDistance = 0;
     mutex dataAccess;
 
 public:
-    Swimmer(string & inName, double inSpeed) : name(inName), speed(inSpeed) {
+    Swimmer(string &inName, double inSpeed) : name(inName), speed(inSpeed) {
         assert(inSpeed > 0);
     }
 
-    static void Swim(Swimmer * swimmer, double distance) {
+    static void Swim(Swimmer *swimmer, double distance, ResultsStorage * results) {
         assert(distance > 0);
         assert(nullptr != swimmer);
+        assert(nullptr != results);
 
-        swimmer->duration = 0;
+        double duration = 0;
         double restOfDistance = distance;
 
         while (restOfDistance > 0) {
-            restOfDistance =  restOfDistance - swimmer->speed;
-            int delay = (restOfDistance > 0) ? 1000 :  1000 - int(fabs(restOfDistance) / swimmer->speed * 1000);
+            restOfDistance = restOfDistance - swimmer->speed;
+            int delay = (restOfDistance > 0) ? 1000 : 1000 - int(fabs(restOfDistance) / swimmer->speed * 1000);
 
             swimmer->dataAccess.lock();
             swimmer->restOfDistance = (restOfDistance > 0) ? restOfDistance : 0;
             swimmer->dataAccess.unlock();
 
-            swimmer->duration += delay;
+            duration += delay;
             this_thread::sleep_for(chrono::milliseconds(delay));
         }
+        results->PushItem(duration);
     }
 
-    static void ShowProgress(const vector<Swimmer *> & swimmers, double distance) {
+    static void ShowProgress(const vector<Swimmer *> &swimmers, double distance) {
         bool isSwimming = true;
-        while(isSwimming) {
+        while (isSwimming) {
             isSwimming = false;
             cout << "---------------------" << endl;
-            for (auto & swimmer : swimmers) {
+            for (auto &swimmer : swimmers) {
                 swimmer->dataAccess.lock();
                 double tempRest = swimmer->restOfDistance;
                 swimmer->dataAccess.unlock();
-                if(tempRest > 0) {
+                if (tempRest > 0) {
                     isSwimming = true;
                 }
                 cout << "Swimmer " << swimmer->name << " swims " << distance - tempRest << " m" << endl;
@@ -122,50 +139,42 @@ public:
         }
     }
 
-    double GetDuration() const {
-        return duration;
-    }
-
-    const string & GetName() {
+    const string &GetName() {
         return name;
     }
 };
 
-bool CompareDuration(const Swimmer * swimmer1, const Swimmer * swimmer2) {
-    return swimmer1->GetDuration() < swimmer2->GetDuration();
-}
-
 int main() {
     vector<Swimmer *> swimmers;
-    thread * calls[NUM_OF_SWIMMERS];
+    thread *calls[NUM_OF_SWIMMERS];
+    ResultsStorage resultsStorage;
 
     for (int i = 0; i < NUM_OF_SWIMMERS; ++i) {
-        string name = GetStringFromStream(string( "Enter swimmer " + to_string(i + 1) + " name:"));
-        double speed = GetDoubleFromStream(string( "Enter swimmer " + to_string(i + 1) + " speed(0 - 20 m/s)"), 0 , 20);
+        string name = GetStringFromStream(string("Enter swimmer " + to_string(i + 1) + " name:"));
+        double speed = GetDoubleFromStream(string("Enter swimmer " + to_string(i + 1) + " speed(0 - 20 m/s)"), 0, 20);
         swimmers.push_back(new Swimmer(name, speed));
     }
 
     int i = 0;
-    for (auto & swimmer : swimmers) {
-        calls[i] = new thread(Swimmer::Swim, swimmer, 100);
+    for (auto &swimmer : swimmers) {
+        calls[i] = new thread(Swimmer::Swim, swimmer, 100, &resultsStorage);
         ++i;
     }
 
     thread progressThread(Swimmer::ShowProgress, swimmers, 100);
 
-    for (auto & call : calls) {
+    for (auto &call : calls) {
         call->join();
     }
 
     progressThread.join();
 
-    sort(swimmers.begin(), swimmers.end(), CompareDuration);
-
     i = 0;
     cout << "---------------------" << endl;
     cout << "Results:" << endl;
-    for (auto & swimmer : swimmers) {
-        cout << "Swimmer " << swimmer->GetName() << ". Time " << swimmer->GetDuration() / 1000 << " s" << endl;
+    auto results = resultsStorage.GetStorage();
+    for (auto &swimmer : swimmers) {
+        cout << "Swimmer " << swimmer->GetName() << ". Time " << results[i] / 1000 << " s" << endl;
         delete swimmer;
         delete calls[i];
         ++i;
